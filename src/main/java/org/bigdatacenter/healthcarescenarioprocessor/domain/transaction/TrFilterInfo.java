0b7ccb21c3d13e71eeb6f374cf682f82be357900
Anthony-Kim <anthonykim1985@gmail.com>
2017-11-22 11:07:53 +0900

    [ADD] transaction domains

---
+++ src/main/java/org/bigdatacenter/healthcarescenarioprocessor/domain/transaction/TrFilterInfo.java
@@ -0,0 +1,19 @@
+package org.bigdatacenter.healthcarescenarioprocessor.domain.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrFilterInfo implements Serializable {
+    private Integer filterUID;
+    private Integer dataSetUID;
+    private String filterName;
+    private String filterEngName;
+    private String filterValues;
+    private String filterOperator;
+}
